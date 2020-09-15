d4d8ae7ca5503d0b240268743852515ef272b794
VSemikoz <slava.semik@yandex.ru>
2020-09-16 02:31:48 +0300

    add DateTypeConverter

---
+++ app/src/main/java/ru/vssemikoz/rickandmortyapp/utils/typeConverters/DateTypeConverter.kt
@@ -0,0 +1,15 @@
+package ru.vssemikoz.rickandmortyapp.utils.typeConverters
+
+import java.text.SimpleDateFormat
+import java.util.*
+
+object DateTypeConverter {
+
+    fun toDate(string: String, format: String): Date? {
+        return SimpleDateFormat(format, Locale.ENGLISH).parse(string)
+    }
+
+    fun toString(date: Date,format: String): String {
+        return SimpleDateFormat(format, Locale.ENGLISH).format(date)
+    }
+}
