6c832c341d2b71be5faae4e0188d4f90eed84ea4
VSemikoz <slava.semik@yandex.ru>
2020-09-16 02:32:36 +0300

    add map(characterItem: CharacterItem): CharacterDetailsItem

--- app/src/main/java/ru/vssemikoz/rickandmortyapp/utils/mappers/CharacterResponseMapper.kt
+++ app/src/main/java/ru/vssemikoz/rickandmortyapp/utils/mappers/CharacterResponseMapper.kt
@@ -1,14 +1,46 @@
 package ru.vssemikoz.rickandmortyapp.utils.mappers
 
 import ru.vssemikoz.rickandmortyapp.model.CharacterApiResponse
+import ru.vssemikoz.rickandmortyapp.model.CharacterDetailsItem
 import ru.vssemikoz.rickandmortyapp.model.CharacterItem
+import ru.vssemikoz.rickandmortyapp.utils.typeConverters.DateTypeConverter
 import javax.inject.Inject
 
 class CharacterResponseMapper @Inject constructor() : CharacterMapper {
     override fun map(response: CharacterApiResponse?): List<CharacterItem> {
-        if(response == null) return listOf()
+        if (response == null) return listOf()
         val result = arrayListOf<CharacterItem>()
         response.newsApiResponseItemList?.forEach { if (it != null) result.add(CharacterItem(it)) }
         return result
     }
+
+    override fun map(characterItem: CharacterItem): CharacterDetailsItem {
+        val fromFormat = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'"
+        val toFormat = "dd MMM yyyy HH:mm:ss"
+        return with(characterItem) {
+            CharacterDetailsItem(
+                name = "Name: ${if (characterItem.name == "") "No name" else characterItem.name}",
+                status = "Status: ${if (characterItem.status == "") "No status" else characterItem.status}",
+                species = "Species: ${if (characterItem.species == "") "No species" else characterItem.species}",
+                type = "Type: ${if (characterItem.type == "") "No type" else characterItem.status}",
+                gender = "Gender: ${if (characterItem.gender == "") "No gender" else characterItem.gender}",
+                imageUrl = imageUrl,
+                episode = "Episodes: ${if (characterItem.episode.isEmpty()) "No episodes"
+                else characterItem.getEpisodesNumbers()}",
+                created = "Created: ${if (characterItem.created == "") "Date unavailable"
+                else characterItem.getFormatDate(fromFormat, toFormat)}"
+            )
+        }
+    }
+
+    fun CharacterItem.getEpisodesNumbers(): String {
+        return episode.joinToString(separator = ", ") { it.substringAfterLast("/") }
+    }
+
+    fun CharacterItem.getFormatDate(fromFormat: String, toFormat: String): String {
+        val date = DateTypeConverter.toDate(this.created, fromFormat)
+        return DateTypeConverter.toString(date!!, toFormat)
+    }
+
+
 }
