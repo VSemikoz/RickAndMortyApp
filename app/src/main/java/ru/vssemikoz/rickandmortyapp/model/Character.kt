456ba3d4544f4ba6047407607edab2a4ef28a519
VSemikoz <slava.semik@yandex.ru>
2020-09-15 13:23:02 +0300

    add character model

---
+++ app/src/main/java/ru/vssemikoz/rickandmortyapp/model/Character.kt
@@ -0,0 +1,12 @@
+package ru.vssemikoz.rickandmortyapp.model
+
+data class Character(
+    val name: String,
+    val status: String,
+    val species: String,
+    val type: String,
+    val gender: String,
+    val imageUrl: String,
+    val episode: String,
+    val created: String
+)
